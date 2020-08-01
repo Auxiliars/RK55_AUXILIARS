4a4f2fbdc5bbcf37707829d7fb7ada4728953a5a
Abhishek Agrawal <abhishekagrawal8888@gmail.com>
2020-08-01 17:31:09 +0530

    Added Imei and unique id for AES encryption

--- Vigilancia-CriminalApp/app/src/main/java/com/example/personaldev/criminialsurveillanceapp/Authentication_Dashboard/Authentication_Dashboard.java
+++ Vigilancia-CriminalApp/app/src/main/java/com/example/personaldev/criminialsurveillanceapp/Authentication_Dashboard/Authentication_Dashboard.java
@@ -1,12 +1,22 @@
 package com.example.personaldev.criminialsurveillanceapp.Authentication_Dashboard;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.telephony.TelephonyManager;
+import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.personaldev.criminialsurveillanceapp.Authentication_Dashboard.background_Verfication.bg_verification;
 import com.example.personaldev.criminialsurveillanceapp.R;
@@ -39,6 +49,17 @@ public class Authentication_Dashboard extends AppCompatActivity {
                 startActivity(new Intent(Authentication_Dashboard.this, com.example.personaldev.criminialsurveillanceapp.Authentication_Dashboard.background_Verfication.bg_verification.class));
             }
         });
+
+        Toast.makeText(this, getUniqueIMEIId(this),Toast.LENGTH_LONG);
+//        String android_id = Settings.Secure.getString(this.getContentResolver(),
+//                Settings.Secure.ANDROID_ID);
+//        Log.i("BRET",android_id);
+
+
+
+        Log.i("BRET", "REACHED");
+
+        Log.i("BRET", getUniqueIMEIId(this));
     }
 
     public void logout(View view) {
@@ -47,5 +68,28 @@ public class Authentication_Dashboard extends AppCompatActivity {
         startActivity(new Intent(Authentication_Dashboard.this, Login_Screen.class));
         finish();
 
+    }
+
+
+    public String getUniqueIMEIId(Context context) {
+        try {
+            TelephonyManager telephonyManager = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
+            if (ActivityCompat.checkSelfPermission(context, Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this ,new String[]{Manifest.permission.READ_PHONE_STATE},123);
+            }
+            String imei = telephonyManager.getDeviceId();
+
+//            String imei = telephonyManager.getImei(1);
+            Log.e("BRET", "=" + imei);
+            if (imei != null && !imei.isEmpty()) {
+                return imei;
+            } else {
+                return android.os.Build.SERIAL;
+            }
+        } catch (Exception e) {
+            String imei = Settings.Secure.getString(getContentResolver(), Settings.Secure.ANDROID_ID);
+            return imei;
+
+        }
     }
 }
